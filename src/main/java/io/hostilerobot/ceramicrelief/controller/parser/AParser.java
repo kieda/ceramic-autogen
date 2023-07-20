0f1567704eba078356466bd265c568e494a96f8f
Zachary Kieda <kieda@users.noreply.github.com>
2023-07-20 16:30:28 -0600

    add name tests

--- src/main/java/io/hostilerobot/ceramicrelief/controller/parser/AParser.java
+++ src/main/java/io/hostilerobot/ceramicrelief/controller/parser/AParser.java
@@ -14,43 +14,6 @@ public interface AParser<T> {
         return false;
     }
 
-    public static int skipWhiteSpace(CharSequence cs, int pos) {
-        while(pos < cs.length() && Character.isWhitespace(cs.charAt(pos))) {
-            pos++;
-        }
-        return pos;
-    }
-    public static int skipNumber(CharSequence cs, int pos) {
-        char current;
-        // include - or + in the number
-        if(pos < cs.length() && ((current = cs.charAt(pos)) == '-' || current == '+'))
-            pos++;
-        while(pos < cs.length() && ((current = cs.charAt(pos)) <= '9') && current >= '0') {
-            pos++;
-        }
-        return pos;
-    }
-    public static int skipName(CharSequence cs, int pos) {
-        if(pos < cs.length()) {
-            // skip first char
-            char current = cs.charAt(pos);
-            if((current <= '9' && current >= '0') || Character.isWhitespace(current)
-                || Arrays.binarySearch(RESERVED_CHARS, current) >= 0) {
-                // illegal first character. Nothing to skip
-                return pos;
-            }
-            pos++; // first character valid. Increment pos
-            while(pos < cs.length()
-                    && !Character.isWhitespace(current = cs.charAt(pos))
-                    && Arrays.binarySearch(RESERVED_CHARS, current) < 0) {
-                // skip till we find an invalid character or string ends
-                pos++;
-            }
-        }
-        return pos;
-    }
-
-
     public static final int RADIX = 10;
     public static final char[] RESERVED_CHARS = CharArrays.sort(
         ',', '=', '/', '\\', '#', '(', ')', '<', '>', '[', ']', '-', ':'
