a4753ca65f98553e7a79af9818faf925d765a07c
Zak <zakkieda@gmail.com>
2025-05-03 01:55:37 -0700

    cool auto testing

--- src/main/java/io/hostilerobot/ceramicrelief/controller/JsonDataProcessor.java
+++ src/main/java/io/hostilerobot/ceramicrelief/controller/JsonDataProcessor.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class JsonDataProcessor<T> implements DataProcessor<T>{
@@ -24,15 +27,18 @@ public class JsonDataProcessor<T> implements DataProcessor<T>{
                 .build();
     }
 
-
     private final Class<T> clazz;
     private final UnaryOperator<JsonNode> extract;
+    private final BiConsumer<T, JsonNode> merging;
+    private final Function<JsonNode, T> mapping;
 
     private JsonNode previous = null;
 
-    private JsonDataProcessor(Class<T> clazz, UnaryOperator<JsonNode> extract) {
+    private JsonDataProcessor(Class<T> clazz, UnaryOperator<JsonNode> extract, BiConsumer<T, JsonNode> merging, Function<JsonNode, T> mapping) {
         this.clazz = clazz;
         this.extract = extract;
+        this.merging = merging;
+        this.mapping = mapping;
     }
 
     private JsonNode extract(InputStream in) throws IOException{
@@ -45,16 +51,23 @@ public class JsonDataProcessor<T> implements DataProcessor<T>{
     public T transform(InputStream in) throws IOException{
         JsonNode input = extract(in);
         previous = input;
-        return mapper.treeToValue(input, clazz);
+        if(mapping != null)
+            return mapping.apply(input);
+        else
+            return mapper.treeToValue(input, clazz);
     }
 
     @Override
     public boolean merge(T current, InputStream in) throws IOException {
         JsonNode input = extract(in);
+        if(merging != null) {
+            merging.accept(current, input);
+        } else {
+            mapper.readerForUpdating(current)
+                    .readValue(input);
+        }
         boolean same = input.equals(previous);
         previous = input;
-        mapper.readerForUpdating(current)
-                .readValue(input);
 
         return !same;
     }
@@ -66,16 +79,30 @@ public class JsonDataProcessor<T> implements DataProcessor<T>{
     public static final class Builder<T> {
         private final Class<T> clazz;
         private UnaryOperator<JsonNode> extract = null;
+        private BiConsumer<T, JsonNode> mergingFunction = null;
+        private Function<JsonNode, T> mappingFunction = null;
 
         private Builder(Class<T> clazz) {
             this.clazz = clazz;
         }
+
+        public Builder<T> customMapping(Function<JsonNode, T> mappingFunction) {
+            this.mappingFunction = mappingFunction;
+            return this;
+        }
+
+        public Builder<T> customMerging(BiConsumer<T, JsonNode> mergingFunction) {
+            this.mergingFunction = mergingFunction;
+            return this;
+        }
+
         public Builder<T> extractFunction(UnaryOperator<JsonNode> extractFunction) {
             this.extract = extractFunction;
             return this;
         }
+
         public JsonDataProcessor<T> build() {
-            return new JsonDataProcessor<>(clazz, extract);
+            return new JsonDataProcessor<>(clazz, extract, mergingFunction, mappingFunction);
         }
     }
 
--- src/main/java/io/hostilerobot/ceramicrelief/drivers/rtee/RTreeTest.java
+++ src/main/java/io/hostilerobot/ceramicrelief/drivers/rtee/RTreeTest.java
@@ -1,11 +1,14 @@
 package io.hostilerobot.ceramicrelief.drivers.rtee;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.hostilerobot.ceramicrelief.controller.DataController;
 import io.hostilerobot.ceramicrelief.controller.JsonDataProcessor;
 import io.hostilerobot.ceramicrelief.controller.TextControllerDirectory;
 import io.hostilerobot.ceramicrelief.controller.TextControllerMatcher;
 import io.hostilerobot.ceramicrelief.drivers.ControllerMain;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -25,15 +28,39 @@ public class RTreeTest extends Application {
 
     private AtomicBoolean shutdown = new AtomicBoolean(false);
 
+
+
     private void fileControllerThread() throws Exception{
         var resource = getClass().getResource("/controller/").getPath();
         // hacky bullshit so we can get the right file
         resource = resource.replace("/target/classes/", "/src/main/resources/");
         TextControllerDirectory.builder()
-                .and(TextControllerMatcher.fileExtension("json"), f ->
-                        DataController.builder(JsonDataProcessor.builder(Map.class).build())
+                .and(TextControllerMatcher.nameAndExtension("rtree_test", "json"), f ->
+                        DataController.builder(
+                                root,
+                                JsonDataProcessor.builder(TriangleMeshView.class)
+                                        .extractFunction(jsonNode -> jsonNode.get("triangles"))
+                                        .customMerging((triangleMesh, jsonNode) ->{
+                                            TriangleView[] items = new TriangleView[jsonNode.size()/3];
+
+                                            for(int i = 0; i < jsonNode.size()-2; i+=3) {
+                                                JsonNode a = jsonNode.get(i);
+                                                JsonNode b = jsonNode.get(i + 1);
+                                                JsonNode c = jsonNode.get(i + 2);
+                                                items[i / 3] = new TriangleView(
+                                                    new Point2D(a.get(0).asDouble(), a.get(1).asDouble()),
+                                                    new Point2D(b.get(0).asDouble(), b.get(1).asDouble()),
+                                                    new Point2D(c.get(0).asDouble(), c.get(1).asDouble())
+                                                );
+                                            }
+
+                                            Platform.runLater(() -> {
+                                                triangleMesh.getTriangleViews().setAll(items);
+                                            });
+                                        })
+                                        .build()
+                                )
                                 .addListener( System.out::println )
-                                .disableMerging()
                                 .build(f))
                 .onError(Throwable::printStackTrace)
                 .onComplete(() -> shutdown.set(true))
@@ -43,11 +70,12 @@ public class RTreeTest extends Application {
         }
     }
 
+    private TriangleMeshView root;
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Test RTree2");
-        TriangleMeshView root = new TriangleMeshView();
-        root.getTriangleViews().add(new TriangleView(new Point2D(50, 50), new Point2D(50, 300), new Point2D(300, 300)));
+        root = new TriangleMeshView();
+//        root.getTriangleViews().add(new TriangleView(new Point2D(50, 50), new Point2D(50, 300), new Point2D(300, 300)));
         Scene scene = new Scene(root);
         scene.onMousePressedProperty().set(mouseEvent -> {
             double x = mouseEvent.getX();
@@ -63,5 +91,12 @@ public class RTreeTest extends Application {
         stage.setHeight(800);
         stage.show();
 
+        new Thread(() -> {
+            try {
+                fileControllerThread();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
